b1a4b2244fe3e8235f1557f59f0a969f34690a10
f3216 <f3216@tomazpelayo.com>
2023-02-01 16:00:01 +0000

    ex2

--- MeuNome.c
+++ MeuNome.c
@@ -3,7 +3,7 @@
 
 int main()
 {
-    printf("\nNome : Carlos Fonseca\n");  
+    printf("Nome : Carlos Fonseca\n");  
     printf("Data : 14 de Setembro de 2007\n");
     printf("Morada : Santo Tirso\n");  
     return (0);
