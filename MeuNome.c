0fac7d931ae3ffc8f331d0fdb7a828781d39fb6b
f3216 <f3216@tomazpelayo.com>
2023-02-01 15:54:26 +0000

    ex

--- MeuNome.c
+++ MeuNome.c
@@ -1,3 +1,11 @@
 // Resolva neste ficheiro o seu exercício
+#include <stdio.h>
 
+int main()
+{
+    printf("\nNome : Carlos Fonseca\n");  
+    printf("Data : 14 de Setembro de 2007\n");
+    printf("Morada : Santo Tirso\n");  
+    return (0);
+}
 
